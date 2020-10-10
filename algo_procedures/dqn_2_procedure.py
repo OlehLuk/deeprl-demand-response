07439e1f645f968f51d1cc1f0e9e960dff80e80a
OlehLuk <oleh.lukianykhin.a@gmail.com>
2020-10-10 20:37:36 +0300

    dqn_2 experiment integration

--- algo_procedures/dqn_2_procedure.py
+++ algo_procedures/dqn_2_procedure.py
@@ -1,78 +1,79 @@
 import numpy as np
 
-from algo_procedures.dqn_2 import DQN2Agent, ReplayMemory, train_helper
+from algo_procedures.dqn_2 import DQN2Agent, ReplayMemory, train_helper, epsilon_annealing
+from pipeline import calc_mse
 from pipeline.experiment import AgentWrapper
 
 
 class DQN_2_Wrapper(AgentWrapper):
-    def __init__(self, max_n_steps):
+    def __init__(self, max_n_steps, input_dim,
+                 output_dim, hidden_dim, capacity,
+                 gamma, batch_size, min_eps,
+                 eps_decrease_last_episode):
+        self.min_eps = min_eps
+        self.eps_decrease_last_episode = eps_decrease_last_episode
+        self.agent = DQN2Agent(input_dim, output_dim, hidden_dim)
+        self.replay_memory = ReplayMemory(capacity)
         self.max_n_steps = max_n_steps
-
-    def play_episode(self, env,
-                     agent: DQN2Agent,
-                     replay_memory: ReplayMemory,
-                     eps: float,
-                     batch_size: int,
-                     gamma=0.99) -> int:
-        """Play an epsiode and train
-        Args:
-            env (gym.Env): gym environment (CartPole-v0)
-            agent (DQN2Agent): agent will train and get action
-            replay_memory (ReplayMemory): trajectory is saved here
-            eps (float): 𝜺-greedy for exploration
-            batch_size (int): batch size
-        Returns:
-            int: reward earned in this episode
-        """
-        s = env.reset()
-        s = np.array(s)
-        done = False
-        total_reward = 0
-        episode_length = 0
-
-        while not done:
-
-            a = agent.get_action(s, eps)
-            s2, r, done, info = env.step(a)
-            s2 = np.array(s2)
-
-            if done:
-                r = -1
-            replay_memory.push(s, a, r, s2, done)
-
-            if len(replay_memory) > batch_size:
-                minibatch = replay_memory.pop(batch_size)
-                train_helper(agent, minibatch, gamma)
-
-            s = s2
-
-        return episode_length
+        self.gamma = gamma
+        self.batch_size = batch_size
+        self.episode_counter = 0
 
     def run_one_train_episode(self, env) -> dict:
-        output = []
         state = env.reset()
-        action = self.agent.use(state)
+        p_actual, p_reference = state
+
+        state = np.array(state)
+        eps = epsilon_annealing(self.episode_counter, self.eps_decrease_last_episode, self.min_eps)
+        action = self.agent.get_action(state, eps)
+        output = {'p_actual': [p_actual], 'p_reference': [p_reference], 'action': [action]}
+
         for i in range(self.max_n_steps):
-            # TODO: save observation to output properly
             next_state, reward, done, _ = env.step(action)
-            output.append(next_state)
-            action = self.agent.learn(state, action, reward, next_state)
+            p_actual, p_reference = next_state
+            output['p_actual'].append(p_actual)
+            output['p_reference'].append(p_reference)
+            output['action'].append(action)
+            next_state = np.array(next_state)
+            self.replay_memory.push(state, action, reward, next_state, done)
+
+            if len(self.replay_memory) > self.batch_size:
+                minibatch = self.replay_memory.pop(self.batch_size)
+                train_helper(self.agent, minibatch, self.gamma)
+
             state = next_state
+            eps = epsilon_annealing(self.episode_counter, self.eps_decrease_last_episode, self.min_eps)
+            action = self.agent.get_action(state, eps)
+
             if done:
                 break
 
+        mse = calc_mse(output['p_actual'], output['p_reference'])
+        output['mse'] = mse
+        self.episode_counter += 1
         return output
 
     def run_one_test_episode(self, env) -> dict:
-        output = []
         state = env.reset()
-        action = self.agent.use(state)
+        p_actual, p_reference = state
+
+        state = np.array(state)
+        eps = self.min_eps
+        action = self.agent.get_action(state, eps)
+        output = {'p_actual': [p_actual], 'p_reference': [p_reference], 'action': [action]}
+
         for i in range(self.max_n_steps):
-            # TODO: save observation to output properly
             next_state, reward, done, _ = env.step(action)
-            output.append(next_state)
-            action = self.agent.use(next_state)
+            p_actual, p_reference = next_state
+            output['p_actual'].append(p_actual)
+            output['p_reference'].append(p_reference)
+            output['action'].append(action)
+            state = np.array(next_state)
+            action = self.agent.get_action(state, eps)
+
             if done:
                 break
 
+        mse = calc_mse(output['p_actual'], output['p_reference'])
+        output['mse'] = mse
         return output
